8c5c85ba91cc95ac045fef63b95d37d1b9bb2fd5
mshandywinata <mshandywinata@upi.edu>
2026-02-16 18:02:38 +0700

    feat fix args position

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 import { toMorse, toAlpha } from "./translator.js";
 
 // get the third and fourth index command as args
-const input = process.argv.slice(2, 3).join(" ");
-const option = process.argv.slice(3, 4).join(" ");
+const option = process.argv.slice(2, 3).join(" ");
+const input = process.argv.slice(3).join(" ");
 
 // raise error
 if (!input || !option) {
-    console.error("Usage: node src/index.js <type your text here> <option: [toMorse or toAlpha]>");
+    console.error("Usage: node src/index.js <input format: [--text or --morse]> <type your input here>");
     process.exit(1);
 }
 
 // branching option
-if (option.toLowerCase() === 'toalpha') {
+if (option.toLowerCase() === "--text") {
     const result = toAlpha(input);
     console.log(result);
-} else if (option.toLowerCase() === "tomorse") {
+} else if (option.toLowerCase() === "--morse") {
     const result = toMorse(input);
     console.log(result);
 } else {
-    console.error("Invalid translation option [toMorse/toAlpha]");
+    console.error("Invalid translation option [--text or --morse]");
     process.exit(1);
 }
